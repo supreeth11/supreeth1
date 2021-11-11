3096e04f8cb49d016bcabb0e1f6f8d03bfa9f974
supreeth11 <supreethzup@gmail.com>
2021-11-11 21:17:32 +0530

    init

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -6,14 +6,26 @@ import { Router } from '@angular/router';
   templateUrl: './customer.component.html',
   styleUrls: ['./customer.component.css']
 })
-export class CustomerComponent implements OnInit {
+export class CustomerComponent  {
+
 
   constructor(private router:Router) { }
-
+  age: any;
+  showAge: any;
   ngOnInit(): void {
   }
+
   next(){
     this.router.navigate(['details']);
   }
 
+
+ageCalculator()
+{
+  if(this.age){
+    const convertAge = new Date(this.age);
+    const timeDiff = Math.abs(Date.now() - convertAge.getTime());
+    this.showAge = Math.floor((timeDiff / (1000 * 3600 * 24))/365);
+  }
+  }
 }
